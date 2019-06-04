c55af32cc7d481a079afd1c5e2d69d308086f767
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 16:42:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -37,8 +37,8 @@ class Pokemon
       #self.new(id, row[1], row[2], db)
       #self.new(id: id, name: row[1], type: row[2], db: db)
       id = row[0]
-      name = row[1]
-      type = row[2]
+      @name = row[1]
+      @type = row[2]
       #binding.pry
       #self.new(id: id, name: name, type: type, db: db)
       
