07ca794712de80070e240336906581757266aee7
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 15:46:15 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,6 +1,6 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
-DB = {:conn => SQLite3::Database.new("db/students.db")}
+DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
 
 CREATE TABLE pokemon (
   id INTEGER PRIMARY KEY,
