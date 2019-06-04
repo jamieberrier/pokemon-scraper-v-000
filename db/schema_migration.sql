366b177e662a3d9a7259ab9caf54b7992f11e37b
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 15:46:58 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,5 +1,8 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+require 'sqlite3'
+require_relative '../lib/student'
+
 DB = {:conn => SQLite3::Database.new("db/pokemon.db")}
 
 CREATE TABLE pokemon (
