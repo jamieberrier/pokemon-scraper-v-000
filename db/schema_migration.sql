dc16739fb4bb35f54c5bb7ef39e98c49b486e8fd
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 16:56:28 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,6 +1,5 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
--- ALTER TABLE pokemon ADD COLUMN hp INTEGER DEFAULT 60;
 CREATE TABLE pokemon (
   id INTEGER PRIMARY KEY,
     name TEXT,
