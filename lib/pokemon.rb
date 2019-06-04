21fab04472e6ef1c2ee4afbde6453a91c9bdc23e
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 16:24:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -31,12 +31,12 @@ class Pokemon
     SQL
 
     db.execute(sql, id).map do |row|
-      #binding.pry
       #self.new(id, row[1], row[2], db)
       #self.new(id: id, name: row[1], type: row[2], db: db)
       name = row[1]
       type = row[2]
     end
+    binding.pry
     self.new(id: id, name: name, type: type, db: db)
   end
 end
