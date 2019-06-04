4a71f55bd19581e5487ab645e88bcbfec1b4508a
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 18:08:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -40,7 +40,7 @@ class Pokemon
     #binding.pry
     #@hp = new_hp
 
-    sql = "UPDATE pokemon SET hp = new_hp WHERE id = ?"
-    db.execute(sql, self.id)
+    sql = "UPDATE pokemon SET hp = ? WHERE id = ?"
+    db.execute(sql, new_hp, self.id)
   end
 end
