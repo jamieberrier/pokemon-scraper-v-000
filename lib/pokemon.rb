dfd22e17f858f8496bf38ec922047235fb493d8b
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 16:40:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -40,9 +40,9 @@ class Pokemon
       name = row[1]
       type = row[2]
       #binding.pry
-      p = self.new(id: id, name: name, type: type, db: db)
+      self.new(id: id, name: name, type: type, db: db)
       #p = self.new
-      binding.pry
+      #binding.pry
     end
     #binding.pry
 
