5fe24060c96d2e7e3ba87dbd248961f12ed688cc
jamieberrier <berrier.jamie@gmail.com>
2019-06-04 15:55:31 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,5 +1,7 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+DB = {:conn => @db}
+
 CREATE TABLE pokemon (
   id INTEGER PRIMARY KEY,
     name TEXT,
